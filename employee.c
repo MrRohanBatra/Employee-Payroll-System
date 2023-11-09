067d6c47528114978b9040a22c9ed9388e75d587
MrRohanBatra <rohanbatra.in@gmail.com>
2023-11-09 20:51:55 +0530

    update

--- employee.c
+++ employee.c
@@ -300,8 +300,8 @@ void display_employees()
         for (int i = 0; i < n; i++) 
         {
             printf("\nEmployee %d:\n", i + 1);
-            printf("Name: %s", emp[i].name);
-            printf("ID:\n%d\n", emp[i].id);
+            printf("Name: %s\n", emp[i].name);
+            printf("ID:%d\n", emp[i].id);
             printf("Department: %s\n", emp[i].dept);
             printf("Gross Salary: %.2f\n", emp[i].salary);
         }
