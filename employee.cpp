960e648acaa93518b6fd0c276a1230e9c59effd8
Rohan Batra <rohanbatra.in@gmail.com>
2023-11-09 20:25:48 +0530

    Update employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -5,76 +5,108 @@
 #include <string.h>
 #include <time.h>
 
-// Define a structure to store employee details
-struct employee {
+//structure to store employee details
+struct employee
+ {
     char name[50]; // Employee name
     int id; // Employee ID
     char dept[20]; // Employee department
     float salary; // Employee gross salary
 };
-
-// Declare a global array of employee structures
+//array declared using structure employee
 struct employee emp[100];
 
-// Declare a global variable to store the number of employees
+// store the number of employees
 int n = 0;
 
-// Declare a function to add a new employee
+//function to welcome the user
+void welcome();
+
+//function to say bye to user
+void bye();
+
+//function to add a new employee
 void add_employee();
 
-// Declare a function to modify an existing employee
+//function to modify an existing employee
 void modify_employee();
 
-// Declare a function to delete an existing employee
+//function to delete an existing employee
 void delete_employee();
 
-// Declare a function to display all the employees
+//function to display all the employees
 void display_employees();
 
-// Declare a function to calculate the net salary of an employee
+// function to calculate the net salary of an employee
 float net_salary(float salary);
 
-// Declare a function to generate the payroll report of an employee
+//function to claculate net salary after cutting the taxes
+float tax(float salary);
+
+//function to generate the payroll report of an employee
 void payroll_report(struct employee e);
 
-// Declare a function to save the employee data to a file
+// Function to generate a payroll slip and save it to a file
+void generate_payroll_slip(struct employee e);
+
+//Declare a function to save the employee data to a file
 void save_data();
 
-// Declare a function to load the employee data from a file
+//function to load the employee data from a file
 void load_data();
 
-// Declare a function to display the main menu
+//Declare a function to display the main menu
 void main_menu();
 
-// Define the main function
-int main() {
+//main function
+int main() 
+{
+    //Welcoming the user
+    welcome();
     // Load the employee data from the file
     load_data();
-
     // Display the main menu
     main_menu();
-
+    //Saying good bye to user
+    bye();
     // Return 0 to indicate normal termination
     return 0;
 }
 
-// Define the function to add a new employee
-void add_employee() {
-    // Declare a local variable to store the choice
-    char ch;
+//function to welcome the user
+void welcome() 
+{
+    printf("Welcome to the Employee Payroll System!\n");
+    printf("Developed by:\n");
+    printf("-> Rohan Batra                                   23104056\n");
+    printf("-> Shubham Rawat                                 231040xx\n");
+    printf("\nPurpose of the App:\n");
+    printf("This application allows you to manage employee payroll information.\n");
+    printf("You can add, modify, delete employees, generate payroll reports, and more.\n\n");
+}
 
-    // Repeat until the user chooses to exit
-    do {
-        // Increment the number of employees
+//function to say bye to user
+void bye()
+{
+    printf("BYE");
+    printf("\nThankyou for using the application");
+    printf("\nFind the project at github:https://github.com/MrRohanBatra/Employee-Payroll-System");
+}
+
+//function to add a new employee
+void add_employee() 
+{
+    char ch;
+    do 
+    {
         n++;
 
-        // Prompt the user to enter the employee details
+        //enter the employee details
         printf("\nEnter the details of employee %d:\n", n);
 
         // Read the employee name
         printf("Name: ");
         scanf("%s", emp[n-1].name);
-
         // Read the employee ID
         printf("ID: ");
         scanf("%d", &emp[n-1].id);
@@ -90,7 +122,8 @@ void add_employee() {
         // Ask the user if they want to add another employee
         printf("\nDo you want to add another employee? (Y/N): ");
         scanf(" %c", &ch);
-    } while (ch == 'Y' || ch == 'y');
+    } 
+    while (ch == 'Y' || ch == 'y');
 
     // Save the employee data to the file
     save_data();
@@ -99,15 +132,14 @@ void add_employee() {
     main_menu();
 }
 
-// Define the function to modify an existing employee
-void modify_employee() {
-    // Declare a local variable to store the employee ID
+//function to modify an existing employee
+void modify_employee() 
+{
     int id;
 
-    // Declare a local variable to store the index
     int index = -1;
 
-    // Declare a local variable to store the choice
+    
     char ch;
 
     // Prompt the user to enter the employee ID
@@ -115,8 +147,10 @@ void modify_employee() {
     scanf("%d", &id);
 
     // Search for the employee with the given ID
-    for (int i = 0; i < n; i++) {
-        if (emp[i].id == id) {
+    for (int i = 0; i < n; i++) 
+    {
+        if (emp[i].id == id) 
+        {
             // Store the index of the employee
             index = i;
             break;
@@ -124,10 +158,13 @@ void modify_employee() {
     }
 
     // Check if the employee is found
-    if (index == -1) {
+    if (index == -1) 
+    {
         // Display an error message
         printf("\nEmployee not found!\n");
-    } else {
+    } 
+    else 
+    {
         // Display the employee details
         printf("\nEmployee details:\n");
         printf("Name: %s\n", emp[index].name);
@@ -140,12 +177,14 @@ void modify_employee() {
         scanf(" %c", &ch);
 
         // Check if the user chooses to modify the employee
-        if (ch == 'Y' || ch == 'y') {
+        if (ch == 'Y' || ch == 'y') 
+        {
             // Prompt the user to enter the new employee details
             printf("\nEnter the new details of employee %d:\n", index + 1);
 
             // Read the new employee name
             printf("Name: ");
+
             scanf("%s", emp[index].name);
 
             // Read the new employee ID
@@ -172,8 +211,9 @@ void modify_employee() {
     main_menu();
 }
 
-// Define the function to delete an existing employee
-void delete_employee() {
+//function to delete an existing employee
+void delete_employee() 
+{
     // Declare a local variable to store the employee ID
     int id;
 
@@ -188,8 +228,10 @@ void delete_employee() {
     scanf("%d", &id);
 
     // Search for the employee with the given ID
-    for (int i = 0; i < n; i++) {
-        if (emp[i].id == id) {
+    for (int i = 0; i < n; i++) 
+    {
+        if (emp[i].id == id) 
+        {
             // Store the index of the employee
             index = i;
             break;
@@ -197,10 +239,13 @@ void delete_employee() {
     }
 
     // Check if the employee is found
-    if (index == -1) {
+    if (index == -1) 
+    {
         // Display an error message
         printf("\nEmployee not found!\n");
-    } else {
+    } 
+    else 
+    {
         // Display the employee details
         printf("\nEmployee details:\n");
         printf("Name: %s\n", emp[index].name);
@@ -213,9 +258,11 @@ void delete_employee() {
         scanf(" %c", &ch);
 
         // Check if the user chooses to delete the employee
-        if (ch == 'Y' || ch == 'y') {
+        if (ch == 'Y' || ch == 'y') 
+        {
             // Delete the employee by shifting the array elements
-            for (int i = index; i < n - 1; i++) {
+            for (int i = index; i < n - 1; i++) 
+            {
                 emp[i] = emp[i + 1];
             }
 
@@ -234,16 +281,21 @@ void delete_employee() {
     main_menu();
 }
 
-// Define the function to display all the employees
-void display_employees() {
-    // Check if there are any employees
-    if (n == 0) {
+//function to display all the employees
+void display_employees() 
+{
+    //if there are any employees
+    if (n == 0) 
+    {
         // Display an error message
         printf("\nNo employees to display!\n");
-    } else {
+    } 
+    else 
+    {
         // Display the employee details
         printf("\nEmployee details:\n");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             printf("\nEmployee %d:\n", i + 1);
             printf("Name: %s\n", emp[i].name);
             printf("ID: %d\n", emp[i].id);
@@ -256,36 +308,64 @@ void display_employees() {
     main_menu();
 }
 
-// Define the function to calculate the net salary of an employee
+//function to calculate the net salary of an employee
+
 float net_salary(float salary) {
-    // Declare a local variable to store the net salary
+    //variable to store the net salary
     float net;
 
-    // Declare a local variable to store the deductions
+    //variable to store the deductions
     float deductions;
 
     // Declare a local variable to store the allowances
     float allowances;
 
-    // Calculate the deductions as 10% of the gross salary
+    //deductions as 10% of the gross salary
     deductions = salary * 0.1;
 
-    // Calculate the allowances as 5% of the gross salary
+    //allowances as 5% of the gross salary
     allowances = salary * 0.05;
     
-        // Calculate the net salary as gross salary minus deductions plus allowances
+    //net salary as gross salary minus deductions plus allowances
     net = salary - deductions + allowances;
 
     // Return the net salary
     return net;
 }
 
-// Define the function to generate the payroll report of an employee
-void payroll_report(struct employee e) {
-    // Declare a local variable to store the net salary
+//function to calculate tax
+float tax(float salary)
+{
+    float tax;
+    //variable to stoe tax
+    if(salary<=500000) 
+    //if salary <50000
+    {
+        tax=0.1*salary; 
+        //tax=10% 
+    }
+    else if(salary>500000 && salary<=1000000) 
+    //salary>500000 && salary<=1000000 
+    {
+        tax=50000+(salary-500000)*0.2; 
+        //tax=50000 +20% of(salary-500000)
+    }
+    else if(salary>100000) 
+    //salary > 1000000
+    {
+        tax=150000+(salary-1000000)*0.3; 
+        //tax=150000 +30% of (salary-1000000)
+    }
+    return tax;
+}
+
+//function to generate the payroll report of an employee
+void payroll_report(struct employee e) 
+{
+    //variable to store the net salary
     float net;
 
-    // Declare a local variable to store the pay date
+    //variable to store the pay date
     char date[20];
 
     // Declare a local variable to store the current time
@@ -294,7 +374,7 @@ void payroll_report(struct employee e) {
     // Get the current time
     t = time(NULL);
 
-    // Format the current time as a date string
+    //Format the current time as a date string
     strftime(date, 20, "%d-%m-%Y", localtime(&t));
 
     // Calculate the net salary of the employee
@@ -307,27 +387,86 @@ void payroll_report(struct employee e) {
     printf("Department: %s\n", e.dept);
     printf("Gross Salary: %.2f\n", e.salary);
     printf("Net Salary: %.2f\n", net);
+    printf("Tax Payable: %.2f\n",tax(net));
     printf("Pay Date: %s\n", date);
 }
 
-// Define the function to save the employee data to a file
-void save_data() {
-    // Declare a local variable to store the file pointer
+// Function to generate a payroll slip and save it to a file
+void generate_payroll_slip(struct employee e) 
+{
+    //variable to store the net salary
+    float net;
+
+    //variable to store the pay date
+    char date[20];
+
+    //variable to store the current time
+    time_t t;
+
+    //Get the current time
+    t = time(NULL);
+
+    //Format the current time as a date string
+    strftime(date, 20, "%d-%m-%Y", localtime(&t));
+
+    //Calculate the net salary of the employee
+    net = net_salary(e.salary);
+
+    //Create a file pointer
     FILE *fp;
 
-    // Open the file in write mode
-    fp = fopen("employee.dat", "w");
+    //Open the file in write mode
+    fp = fopen("payroll_slip.txt", "w");
 
-    // Check if the file is opened successfully
-    if (fp == NULL) {
+    //Check if the file is opened successfully
+    if (fp == NULL) 
+    {
         // Display an error message
         printf("\nError in opening the file!\n");
-    } else {
+    } 
+    else 
+    {
+        // Write the payroll slip details to the file
+        fprintf(fp, "Payroll Slip of Employee %d\n", e.id);
+        fprintf(fp, "Name: %s\n", e.name);
+        fprintf(fp, "ID: %d\n", e.id);
+        fprintf(fp, "Department: %s\n", e.dept);
+        fprintf(fp, "Gross Salary: %.2f\n", e.salary);
+        fprintf(fp, "Net Salary: %.2f\n", net);
+        fprintf(fp, "Tax Payable: %.2f\n",tax(net));
+        fprintf(fp, "Pay Date: %s\n", date);
+
+        // Close the file
+        fclose(fp);
+
+        // Display a success message
+        printf("\nPayroll Slip generated and saved to 'payroll_slip.txt'!\n");
+    }
+}
+
+//function to save the employee data to a file
+void save_data() 
+{
+    //variable to store the file pointer
+    FILE *fp;
+
+    //Open the file in write mode
+    fp = fopen("employee.dat", "w");
+
+    //Check if the file is opened successfully
+    if (fp == NULL) 
+    {
+        // Display an error message
+        printf("\nError in opening the file!\n");
+    }
+    else 
+    {
         // Write the number of employees to the file
         fprintf(fp, "%d\n", n);
 
         // Write the employee details to the file
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             fprintf(fp, "%s %d %s %.2f\n", emp[i].name, emp[i].id, emp[i].dept, emp[i].salary);
         }
 
@@ -339,24 +478,29 @@ void save_data() {
     }
 }
 
-// Define the function to load the employee data from a file
-void load_data() {
-    // Declare a local variable to store the file pointer
+//function to load the employee data from a file
+void load_data() 
+{
+    //variable to store the file pointer
     FILE *fp;
 
     // Open the file in read mode
     fp = fopen("employee.dat", "r");
 
     // Check if the file is opened successfully
-    if (fp == NULL) {
+    if (fp == NULL) 
+    {
         // Display an error message
         printf("\nError in opening the file!\n");
-    } else {
+    } 
+    else 
+    {
         // Read the number of employees from the file
         fscanf(fp, "%d", &n);
 
         // Read the employee details from the file
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             fscanf(fp, "%s %d %s %f", emp[i].name, &emp[i].id, emp[i].dept, &emp[i].salary);
         }
 
@@ -368,9 +512,10 @@ void load_data() {
     }
 }
 
-// Define the function to display the main menu
-void main_menu() {
-    // Declare a local variable to store the choice
+//function to display the main menu
+void main_menu() 
+{
+    //variable to store the choice
     int choice;
 
     // Display the main menu
@@ -379,39 +524,53 @@ void main_menu() {
     printf("2. Modify Employee\n");
     printf("3. Delete Employee\n");
     printf("4. Display Employees\n");
-    printf("5. Generate Payroll Report\n");
-    printf("6. Exit\n");
+    printf("5. Tax Payable\n");
+    printf("6. Generate Payroll Report\n");
+    printf("7. Generate Payroll Slip\n");
+    printf("8. Exit\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
     int id,index;
 
     // Switch on the choice
-    switch (choice) {
+    switch (choice) 
+    {
         case 1:
+        {
             // Call the function to add a new employee
             add_employee();
             break;
+        }
         case 2:
+        {
             // Call the function to modify an existing employee
             modify_employee();
             break;
+        }
         case 3:
+        {
             // Call the function to delete an existing employee
             delete_employee();
             break;
+        }
         case 4:
+        {
             // Call the function to display all the employees
             display_employees();
             break;
+        }
         case 5:
-            index = -1;
+        {
+          index = -1;
             // Prompt the user to enter the employee ID
-            printf("\nEnter the ID of the employee for payroll report: ");
+            printf("\nEnter the ID of the employee for tax report: ");
             scanf("%d", &id);
 
             // Search for the employee with the given ID
-            for (int i = 0; i < n; i++) {
-                if (emp[i].id == id) {
+            for (int i = 0; i < n; i++) 
+            {
+                if (emp[i].id == id) 
+                {
                     // Store the index of the employee
                     index = i;
                     break;
@@ -419,10 +578,49 @@ void main_menu() {
             }
 
             // Check if the employee is found
-            if (index == -1) {
+            if (index == -1) 
+            {
                 // Display an error message
                 printf("\nEmployee not found!\n");
-            } else {
+            } 
+            else 
+            {
+                //calculate the tax
+                float tax1=tax(net_salary(emp[index].salary));
+                // Call the function to generate the payroll report of the employee
+                printf("Tax Payable:%.2f",tax1);
+            }
+
+            // Display the main menu
+            main_menu();
+            break;
+        }
+        case 6:
+        {
+              index = -1;
+            // Prompt the user to enter the employee ID
+            printf("\nEnter the ID of the employee for payroll report: ");
+            scanf("%d", &id);
+
+            // Search for the employee with the given ID
+            for (int i = 0; i < n; i++) 
+            {
+                if (emp[i].id == id) 
+                {
+                    // Store the index of the employee
+                    index = i;
+                    break;
+                }
+            }
+
+            // Check if the employee is found
+            if (index == -1) 
+            {
+                // Display an error message
+                printf("\nEmployee not found!\n");
+            } 
+            else 
+            {
                 // Call the function to generate the payroll report of the employee
                 payroll_report(emp[index]);
             }
@@ -430,16 +628,55 @@ void main_menu() {
             // Display the main menu
             main_menu();
             break;
-        case 6:
+        }
+        case 7:
+        {
+             index = -1;
+            // Prompt the user to enter the employee ID
+            printf("\nEnter the ID of the employee for payroll report: ");
+            scanf("%d", &id);
+
+            // Search for the employee with the given ID
+            for (int i = 0; i < n; i++) 
+            {
+                if (emp[i].id == id) 
+                {
+                    // Store the index of the employee
+                    index = i;
+                    break;
+                }
+            }
+
+            // Check if the employee is found
+            if (index == -1) 
+            {
+                // Display an error message
+                printf("\nEmployee not found!\n");
+            } 
+            else 
+            {
+                // Call the function to generate the payroll report of the employee
+                generate_payroll_slip(emp[index]);
+            }
+
+            // Display the main menu
+            main_menu();
+            break;
+        }
+        case 8:
+        {
             // Exit the program
             exit(0);
             break;
+        }
         default:
+        {
             // Display an error message
             printf("\nInvalid choice!\n");
 
             // Display the main menu
             main_menu();
             break;
+        }
     }
 }
