12665d92da422b4f67d2955b5fdd46934b7c90b3
MrRohanBatra <rohanbatra.in@gmail.com>
2023-11-09 20:57:41 +0530

    update

--- employee.c
+++ employee.c
@@ -473,7 +473,10 @@ void save_data()
         // Write the employee details to the file
         for (int i = 0; i < n; i++) 
         {
-            fprintf(fp, "%s\n%d\n%s\n%.2f\n", emp[i].name, emp[i].id, emp[i].dept, emp[i].salary);
+            //fprintf(fp, "%s\n%d\n%s\n%.2f\n", emp[i].name, emp[i].id, emp[i].dept, emp[i].salary);
+            fprintf(fp,"%s\n",emp[i].name);
+            fprintf(fp,"%d\n",emp[i].id);
+        
         }
 
         // Close the file
